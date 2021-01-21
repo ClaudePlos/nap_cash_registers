457ade11b30ba725d65f71874d627e8ee6d572de
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-01-21 12:35:09 +0100

    Initial commit

---
+++ src/main/java/kskowronski/views/cashregister/CashRegisterView.java
@@ -0,0 +1,18 @@
+package kskowronski.views.cashregister;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import kskowronski.views.main.MainView;
+
+@Route(value = "cashregister", layout = MainView.class)
+@PageTitle("Cash Register")
+@CssImport("./styles/views/helloworld/hello-world-view.css")
+@RouteAlias(value = "", layout = MainView.class)
+public class CashRegisterView extends HorizontalLayout {
+
+
+
+}
