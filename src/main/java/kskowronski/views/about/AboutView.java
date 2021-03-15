054846d8442e82a0133d4b575b066edbb94c88d9
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-03-15 10:22:13 +0100

    add link to analysis

--- src/main/java/kskowronski/views/about/AboutView.java
+++ src/main/java/kskowronski/views/about/AboutView.java
@@ -1,6 +1,8 @@
 package kskowronski.views.about;
 
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,7 +14,11 @@ public class AboutView extends Div {
 
     public AboutView() {
         setId("about-view");
-        add(new Text("Content placeholder"));
+        add(new Text("Content: "), new Html("<div><br></div>"));
+
+        Anchor anchor = new Anchor("https://github.com/ClaudePlos/nap_cash_registers/issues/2", "Analiza");
+        anchor.setTarget("_blank");
+        add(anchor);
     }
 
 }
