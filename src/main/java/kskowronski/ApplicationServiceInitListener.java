558513fee1fa2b59a2741fabbf08e59df2828a77
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-03-18 15:09:42 +0100

    added to index.html attribute notraslate

--- src/main/java/kskowronski/ApplicationServiceInitListener.java
+++ src/main/java/kskowronski/ApplicationServiceInitListener.java
@@ -4,24 +4,27 @@ import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringComponent
 public class ApplicationServiceInitListener implements VaadinServiceInitListener {
+    static Logger logger = Logger.getLogger(ApplicationServiceInitListener.class.getName());
 
     @Override
     public void serviceInit(ServiceInitEvent event) {
 
-        event.addIndexHtmlRequestListener(indexHtmlResponse -> {
-            System.out.println(indexHtmlResponse);
-            indexHtmlResponse.getDocument().getElementsByAttribute("lang").addClass("notranslate\" translate=\"no");
-        });
+        event.addIndexHtmlRequestListener(indexHtmlResponse ->
+            indexHtmlResponse.getDocument().getElementsByAttribute("lang")
+                    .addClass("notranslate")
+                    .attr("translate","no")
+        );
 
         event.getSource().addSessionInitListener( sessionInitEvent -> {
-            //System.out.println("Init session");
+
         });
 
-        event.getSource().addServiceDestroyListener( destroyEvent -> {
-            System.out.println("destroyEvent");
-        });
+        event.getSource().addServiceDestroyListener( destroyEvent -> logger.log(Level.INFO, "destroyEvent"));
 
     }
 }
